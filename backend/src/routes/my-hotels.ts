0ea829a3ddce686e5eaaf1b2c78bff9dd8f1f093
trinhsang02 <22521243@gm.uit.edu.vn>
2024-03-28 22:15:54 +0700

    add router.put backend

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -91,6 +91,43 @@ router.get("/:id", verifyToken, async (req: Request, res: Response) => {
   }
 });
 
+
+router.put(
+  "/:hotelId", 
+  verifyToken, 
+  upload.array("imageFiles"), 
+  async(req: Request, res: Response) => {
+    try{
+      const updatedHotel: HotelType = req.body;
+      updatedHotel.lastUpdated = new Date();
+      
+      const hotel = await Hotel.findOneAndUpdate({
+        _id: req.params.hotelId,
+        userId: req.userId,
+      },
+      updatedHotel,
+      {new: true}
+      );
+
+      if(!hotel) {
+        return res.status(404).json({ message: "Hotel not found"});
+      }
+
+      const files = req.files as Express.Multer.File[];
+      const updatedImageUrls = await uploadImages(files);
+
+      hotel.imageUrls = [
+        ..updatedImageUrls, 
+        ...(updatedHotel.imageUrls || [])
+      ];
+
+      await hotel.save();
+    }catch(error){
+      res.status(500).json({message: "Something went throw"})
+    }
+  }
+);
+
 async function uploadImages(imageFiles: Express.Multer.File[]) {
   const uploadPromises = imageFiles.map(async (image) => {
     const b64 = Buffer.from(image.buffer).toString("base64");
@@ -102,5 +139,6 @@ async function uploadImages(imageFiles: Express.Multer.File[]) {
   const imageUrls = await Promise.all(uploadPromises);
   return imageUrls;
 }
+
 
 export default router;
